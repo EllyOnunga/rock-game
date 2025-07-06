62ffc76ae3f8ce8d07e877095555df63537603b4
Elly Onunga <onungaelly@gmail.com>
2025-07-06 10:47:03 +0300

    Add script.js

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
 function play(playerChoice) {
     const choices = ['rock', 'paper', 'scissors'];
-    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-    let result;
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
 
-    if (playerChoice === computerChoice) {
-        result = "It's a tie!";
-    } else if (
-        (playerChoice === 'rock' && computerChoice === 'scissors') ||
-        (playerChoice === 'paper' && computerChoice === 'rock') ||
-        (playerChoice === 'scissors' && computerChoice === 'paper')
-    ) {
-        result = "You win!";
-    } else {
-        result = "You lose!";
+    document.getElementById('player-choice').
+    textContent = `You chose: ${playerChoice}`;
+
+    document.getElementById('computer-choice').
+    textContent = `Computer chose: ${computerChoice}`;
+
+
+    let result = getWinner(playerChoice, computerChoice);
+    document.getElementById('winner').textContent = `Result: ${result}`;
+}
+
+function getWinner(player, computer) {
+    if (player === computer) {
+        return "It's a tie!";
     }
 
-    document.getElementById('result').innerText = `You chose ${playerChoice}, 
-    computer chose ${computerChoice}. ${result}`;
+    if (
+        (player === 'rock' && computer === 'scissors') ||
+        (player === 'paper' && computer === 'rock') ||
+        (player === 'scissors' && computer === 'paper')
+    ) {
+        return "You win!";
+    }
+
+    return "You lose!";
 }
